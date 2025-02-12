a999e16c12a3bd3874023b08e72f8a9bcf312653
anitabaron <111062951+anitabaron@users.noreply.github.com>
2025-02-12 20:55:58 +0100

    reconstruction of content

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,19 @@
-export type Category = "general" | "work" | "sport" | "hobby";
+export enum Category {
+  GENERAL = "general",
+  WORK = "work",
+  SPORT = "sport",
+  HOBBY = "hobby",
+}
+
+// poniższe są równoważne
 export interface Task {
   name: string;
   done: boolean;
   category?: Category;
 }
+
+export type TaskType = {
+  name: string;
+  done: boolean;
+  category?: Category;
+};
